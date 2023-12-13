cd7d822e8223918cbb13564ac29534e192b0d882
channy <ox043440@gmail.com>
2023-12-13 18:17:51 +0900

    231213

---
+++ blog/src/components/main/main.jsx
@@ -0,0 +1,14 @@
+export const Main = () => {
+    return (
+        <>
+            <div className={styles.filter}>
+                <p>최신순</p>
+                <p>낮은 가격</p>
+                <p>높은 가격</p>
+            </div>
+            <main className={styles.flex_wrap}>
+                
+            </main>
+        </>
+    )
+}
