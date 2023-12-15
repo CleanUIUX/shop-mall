ca4622c00362bb43a00acbe94ee0a8ba18853f36
channy <ox043440@gmail.com>
2023-12-15 18:12:51 +0900

    231215

--- blog/src/components/product_detail/detail.jsx
+++ blog/src/components/product_detail/detail.jsx
@@ -9,7 +9,42 @@ export const Detail = () => {
                         <img src="/images/image002.png" alt="product" />
                     </div>
                 </section>
-                <section></section>
+                <section className={styles.product}>
+                    <div className={styles.product_info}>
+                        <p className={styles.seller_store}>아이돈케어</p>
+                        <p className={styles.product_name}>마로네</p>
+                        <span className={styles.price}>
+                            1000
+                            <span className={styles.unit}>원</span>
+                        </span>
+                    </div>
+
+                    <div className={styles.delivery}>
+                        <p>택배 / 무료 배송</p>
+                    </div>
+
+                    <div className={styles.line}></div>
+
+                    <div className={styles.amount}>
+                        <img
+                            className={styles.minus} 
+                            src="/images/icon-minus-line.svg" 
+                            alt="minus" 
+                        />
+
+                        <div className={styles.count}>
+                            <>1</>
+                        </div>
+
+                        <img
+                            className={styles.plus} 
+                            src="/images/icon-plus-line.svg" 
+                            alt="plus" 
+                        />
+                    </div>
+
+                    
+                </section>
             </main>
         </>
     )
